0200a5daaf397d61f9e573a152e2f5b087615622
GreyMatter's <greymattersyt@gmail.com>
2023-02-20 20:06:46 +0530

    Create  Bulk_email_sender.py

---
+++ Bulk_email_sender.py
@@ -0,0 +1,30 @@
+#impoet the necessary modules
+import smtplib, ssl, csv
+from email.message import EmailMessage
+
+sender = '' #add your sender email address
+password = '' #add your app password
+
+subject = '' #add the subject #add the subject to your email
+body_message = '' #type the message you want to send
+
+#connect to our outgoing mail SMTP server
+context = ssl.create_default_context()
+server = smtplib.SMTP_SSL('smtp.gmail.com', 'Port for SSL', context = context)
+
+server.login(sender, password)
+
+#The formula we will use to send emails
+with open('csv file path', 'r') as csvfile:
+    datareader = csv.reader(csvfile)
+    for row in datareader:
+        em = EmailMessage()
+        em['From'] = sender
+        em['To'] = row
+        em['Subject'] = subject
+        em.set_content(body_message)
+        server.send_message(em)
+        print("The message sent")
+
+server.closed()
+print("Done @GreyMatter_Bots")
